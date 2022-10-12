3e70c8c7b1a99b015a0c34e1f8d35786b7596f47
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-12 19:52:38 +0300

    Updated the index.js server file to connect to the Database (locally)

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
+const config = require('./config');
 const PORT = 3001;
 
 const app = express();
@@ -7,5 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+app.get('/', async function (req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db);
+        res.status(200).send('Database connection successful');
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+})
 
 app.listen(PORT);
