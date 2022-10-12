6da6d5c1185aed132e4049b9f37e778ee2fb3848
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-12 19:17:55 +0300

    Created a simple nodeJS express server in a separate folder

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const PORT = 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(PORT);
