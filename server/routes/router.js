6f6a226a40f2b3c1912842afbc226f41e595cd78
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-19 14:43:16 +0300

    Bugfixed router.get and router.delete function. Router.get -> forgot to execute called function. Router.delete -> reference error for id retrieval.

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,7 @@ const dbFunctions = require('../services/functions');
 
 router.get('/', async function (req, res, next) {
     try {
-        res.status(200).json(await dbFunctions.getAllTasks);
+        res.status(200).json(await dbFunctions.getAllTasks());
     } catch (error) {
         next(error);
     }
@@ -20,7 +20,7 @@ router.post('/new', async function (req, res, next) {
 
 router.delete('/delete/:id', async function (req, res, next) {
     try {
-        res.status(200).json(await dbFunctions.removeTask(req.body.id));
+        res.status(200).json(await dbFunctions.removeTask(req.params.id));
     } catch (error) {
         next(error);
     }
