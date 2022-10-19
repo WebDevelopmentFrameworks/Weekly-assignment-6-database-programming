412f7b9a2102ce17a0299fe6706f434a49984990
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-19 14:21:58 +0300

    Created functionality, to handle routing

---
+++ server/routes/router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const dbFunctions = require('../services/functions');
+
+router.get('/', async function (req, res, next) {
+    try {
+        res.status(200).json(await dbFunctions.getAllTasks);
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('/new', async function (req, res, next) {
+    try {
+        res.status(200).json(await dbFunctions.addTask(req.body));
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.delete('/delete/:id', async function (req, res, next) {
+    try {
+        res.status(200).json(await dbFunctions.removeTask(req.body.id));
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.put('/edit', async function (req, res, next) {
+    try {
+        res.status(200).json(await dbFunctions.updateTask(req.body));
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = router;
