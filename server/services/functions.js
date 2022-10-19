7924cb87e5158ea43399cc00c8cb34453a7287e9
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-10-19 14:10:33 +0300

    Added all needed db functions for CRUD.

--- server/services/functions.js
+++ server/services/functions.js
@@ -12,3 +12,19 @@ async function addTask(task) {
     return task;
 }
 
+async function removeTask(id) {
+    const result = await db.query(`delete from task where id = ${id}`);
+    return id;
+}
+
+async function updateTask(task) {
+    const result = await db.query(`update task set description='${task.description}' where id='${task.id}'`);
+    return task;
+}
+
+module.exports = {
+    getAllTasks,
+    addTask,
+    removeTask,
+    updateTask
+}
